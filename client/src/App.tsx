5acf0f9e0e72156d40b36212381151fa74594a99
MADANA-GS <madangsnaik@gmail.com>
2026-06-11 22:18:55 +0530

    feat: implement user authentication flow with react-query; add API calls for user data and enhance login/register functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Protected from "./ProtectedRoutes/Protected";
 import Topic from "./modules/topic/Topic";
+import { useQuery } from "@tanstack/react-query";
+import { me } from "./modules/auth/auth.api";
+import type { AxiosError } from "axios";
+import toast from "react-hot-toast";
+import type { User } from "./modules/user/user.types";
+import { useAppDispatch } from "./app/hook";
+import { clearAuthenticated, setAuthenticated } from "./app/slice/authSlice";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useAppDispatch();
+  const authQuery = useQuery({
+    queryKey: ["auth"],
+    queryFn: me,
+    retry: 3,
+    refetchOnWindowFocus: true,
+    refetchOnMount: true,
+    refetchOnReconnect: true,
+  });
+
+  useEffect(() => {
+    if (authQuery.isSuccess) {
+      console.log("User data fetched successfully:", authQuery.data);
+      dispatch(setAuthenticated(authQuery.data as User));
+      navigate("/dashboard");
+    } else if (authQuery.isError) {
+      toast.error("Failed to Authenticate! Please Re login");
+      dispatch(clearAuthenticated());
+      navigate("/");
+    }
+  }, [authQuery.isError, authQuery.isSuccess]);
+
   return (
     <div className="bg-white h-screen w-screen overflow-hidden">
       <Routes>
--- client/src/modules/auth/auth.api.ts
+++ client/src/modules/auth/auth.api.ts
@@ -6,13 +6,28 @@ import axios from "axios";
 const API_URL: string = String(import.meta.env.VITE_BACKEND_URL);
 
 export const login = async (payload: LoginPayload): Promise<User> => {
-  const response = await axios.post(`${API_URL}/api/v1/auth/login`, payload);
+  const response = await axios.post(`${API_URL}/api/v1/auth/login`, payload, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 export const register = async (payload: RegisterPayload): Promise<User> => {
-  const response = await axios.post(`${API_URL}/api/v1/auth/register`, payload);
+  const response = await axios.post(
+    `${API_URL}/api/v1/auth/register`,
+    payload,
+    {
+      withCredentials: true,
+    },
+  );
   return response.data;
 };
 
-export const logout = async () => {};
+export const me = async (): Promise<User | undefined> => {
+  const response = await axios.get(`${API_URL}/api/v1/auth/me`, {
+    withCredentials: true,
+  });
+
+  console.log("Me response:", response.data);
+  return response.data;
+};
