8fafec395320d86efeaccf559e62eed978c7effc
MADANA-GS <madangsnaik@gmail.com>
2026-06-13 01:41:37 +0530

    refactor: remove server configuration from Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,14 +4,5 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      clientPort: 443,
-    },
-    allowedHosts: true,
-  },
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
